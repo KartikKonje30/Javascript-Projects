cd76507fa2dfd734ce43c2a5d3ad698b921e6f2a
KartikKonje30 <kartikkonje.01@gmail.com>
2024-07-23 18:41:34 +0530

    string

---
+++ JavaScript Practice/string.js
@@ -0,0 +1,10 @@
+
+const name = "Kartik"
+
+// console.log(name + " Konje");
+
+//use string interpolation
+// console.log(`My name is ${name} Konje.`);
+
+const str1 = new String("Kartik Konje")
+console.log(str1);
