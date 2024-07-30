b5bdab3b46b8ee8b1842f4c19cbcb89a8dfe1b66
KartikKonje30 <kartikkonje.01@gmail.com>
2024-07-30 17:27:56 +0530

    math & num

--- JavaScript Practice/string.js
+++ JavaScript Practice/string.js
@@ -7,4 +7,9 @@ const name = "Kartik"
 // console.log(`My name is ${name} Konje.`);
 
 const str1 = new String("Kartik Konje")
-console.log(str1);
+// console.log(str1[0])
+// console.log(str1.__proto__);
+
+
+// String Methods which are mostly known an used
+// 
